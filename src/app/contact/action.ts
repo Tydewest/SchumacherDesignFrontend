e48f37aa4c836c3f62f27a6dc6980e17b52db24e
Lachy Schuamcher <lachy@tydewest.com>
2025-05-18 16:18:03 +1000

    Update action.ts

--- src/app/contact/action.ts
+++ src/app/contact/action.ts
@@ -9,6 +9,7 @@ export default async function SendWebEnquiry(formData: FormData) {
     const phone = formData.get("phone");
     const subject = formData.get("subject");
     const message = formData.get("message");
+    const device = fromData.get("device");
     const userAgent = formData.get("userAgent")
 
 
@@ -22,7 +23,7 @@ export default async function SendWebEnquiry(formData: FormData) {
             from: 'LSD Web Form<WebForm@dynaserve.io>',
             to: 'WebMessages@dynaserve.io',
             subject: 'New Form Submission Received',
-            html: `<strong>New Web Form Submission Received</strong><dl><dt><b>Name:</b><dd><i>${name}</i><dt><b>Email:</b><dd><i>${email}</i><dt><b>Phone:</b><dd><i>${phone}</i><dt><b>Subject:</b><dd><i>${subject}</i><dt><b>Message:</b><dd><i>${message}</i></dl><b>User Agent:</b><dd><i>${userAgent}</i></dl>`,
+            html: `<strong>New Web Form Submission Received</strong><dl><dt><b>Name:</b><dd><i>${name}</i><dt><b>Email:</b><dd><i>${email}</i><dt><b>Phone:</b><dd><i>${phone}</i><dt><b>Subject:</b><dd><i>${subject}</i><dt><b>Message:</b><dd><i>${message}</i></dl><b>User Metadata:</b><dd><i>${device}</i><br/><i>${userAgent}</dl>`,
           }),
         });
         revalidatePath('/contact')
